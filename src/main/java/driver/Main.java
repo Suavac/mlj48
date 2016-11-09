d509567c169f97e5cf9a98ce1ac5e11453018e85
Slawek <s.mlynarowicz1@nuigalway.ie>
2016-11-09 00:09:04 +0000

    New Entropy

--- src/main/java/driver/Main.java
+++ src/main/java/driver/Main.java
@@ -1,13 +1,5 @@
 package driver;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
 import java.util.*;
 
 /**
@@ -16,11 +8,11 @@ import java.util.*;
 public class Main {
 
     public static void main(final String... args) throws Exception {
-
-        PreprocessData ppd = new PreprocessData("owls15.csv");
+///http://clear-lines.com/blog/post/Discretizing-a-continuous-variable-using-Entropy.aspx
+        final PreprocessData ppd = new PreprocessData("owls15.csv");
         final HashMap<String, Attribute> attributes = ppd.getAttributes();
         ppd.getDecisionClass();
-        final String attributeName = ppd.getAttributeNames().get(0);
+        final String attributeName = ppd.getAttributeNames().get(2);
         final Attribute a = attributes.get(attributeName);
         final Attribute target = attributes.get("type");
         a.convertCont();
@@ -33,10 +25,10 @@ public class Main {
         final ArrayList<Float> bContin = a.getValuesContinous();
 
         final ArrayList<String> testingDiscretization = new ArrayList<String>();
-        for(Float value : bContin){
-            if(value<a.getThresholds().get(0)){
+        for (final Float value : bContin) {
+            if (value < a.getThresholds().get(7)) {
                 testingDiscretization.add("A");
-            }else{
+            } else {
                 testingDiscretization.add("B");
             }
         }
@@ -46,50 +38,94 @@ public class Main {
         calculateGainForPair(target.getValues(), testingDiscretization);
     }
 
-    public static Float calculateGainForPair(ArrayList<String> target, ArrayList<String> attribute){
-        float occurancessAB = attribute.size();
-        float occurrencesA = Collections.frequency(attribute, "A");
-        float occurrencesB = Collections.frequency(attribute, "B");
-        float probA = occurrencesA/occurancessAB;
-        float probB = occurrencesB/occurancessAB;
+    public static Float calculateGainForPair(final ArrayList<String> target, final ArrayList<String> attribute) {
+        final float occurancessAB = attribute.size();
+        final float occurrencesA = Collections.frequency(attribute, "A");
+        final float occurrencesB = Collections.frequency(attribute, "B");
+        final float probA = occurrencesA / occurancessAB;
+        final float probB = occurrencesB / occurancessAB;
+        final HashMap<String, Float> a = countOcc(target, attribute, "A");
+        final HashMap<String, Float> b = countOcc(target, attribute, "B");
+        System.out.println("occurancess AB = " + occurancessAB);
+        System.out.println("occurrences A = " + occurrencesA);
+        System.out.println("occurrences B = " + occurrencesB);
+        System.out.println("probability A = " + probA);
+        System.out.println("probability B = " + probB);
+        System.out.println("OCCURANCES MATRIX A = " + a);
+        System.out.println("OCCURANCES MATRIX B = " + b);
 
-        final HashMap<String, Long> countMap = new HashMap<String, Long>();
-        // count occurances of decision classes
-        int i = 0;
-        for(String label : target){
-            if(attribute.get(i).equals("B")){
-                if (!countMap.containsKey(label)) {
-                    countMap.put(label, 1L);
-                } else {
-                    Long count = countMap.get(label);
-                    count = count + 1;
-                    countMap.put(label, count);
-                }
-//            } else if(attribute.get(i).equals("B")){
-//                if (!countMap.containsKey(label)) {
-//                    countMap.put(label, 1L);
-//                } else {
-//                    Long count = countMap.get(label);
-//                    count = count + 1;
-//                    countMap.put(label, count);
-//                }
-            }
-            i++;
-        }
-        System.out.println("OCCURANCES MATRIX = " + countMap);
+        //calculate entropy for each A or B
+        float entropy = 0;
+        final float occA = a.get("SnowyOwl") + a.get("BarnOwl") + a.get("LongEaredOwl");
+        final float probOccA = occA / occurancessAB;
+        //System.out.println(occA);
+        //entropy -= (1 * (Math.log(1) / Math.log(2)));
+        //entropy -= (0 * (Math.log(0) / Math.log(2)));
+        //entropy -= (0 * (Math.log(0) / Math.log(2)));
+        entropy *= probOccA;
+        System.out.println("ENTROPY 1: " + entropy);
+
+        float entropy2 = 0;
+        final float occB = b.get("SnowyOwl") + b.get("BarnOwl") + b.get("LongEaredOwl");
+        final float probOccA2 = occB / occurancessAB;
+        //System.out.println(occA);
+        // final float r = b.get("SnowyOwl") / (occurancessAB - 1);
+        entropy2 -= (b.get("SnowyOwl") / (occB) * (Math.log(b.get("SnowyOwl") / (occB)) / Math.log(2)));
+        entropy2 -= (b.get("BarnOwl") / (occB) * (Math.log(b.get("BarnOwl") / (occB)) / Math.log(2)));
+        entropy2 -= (b.get("LongEaredOwl") / (occB) * (Math.log(b.get("LongEaredOwl") / (occB)) / Math.log(2)));
+        System.out.println("ENTROPY before prob 2: " + entropy2);
+        entropy2 *= probOccA2;
+        System.out.println("Prob 2: " + probOccA2);
+        System.out.println("ENTROPY 2: " + entropy2);
+
+        final float occuranceReal1 = 45;
+        final float occuranceRealAll = 45;
+
+        float entropy3 = 0;
+        entropy3 -= (occuranceReal1 / (occurancessAB) * (Math.log(occuranceReal1 / (occurancessAB)) / Math.log(2)));
+        entropy3 -= (occuranceReal1 / (occurancessAB) * (Math.log(occuranceReal1 / (occurancessAB)) / Math.log(2)));
+        entropy3 -= (occuranceReal1 / (occurancessAB) * (Math.log(occuranceReal1 / (occurancessAB)) / Math.log(2)));
+
+        System.out.println("ENTROPY Target: " + entropy3);
+        System.out.println("Gain Target 1 : " + (entropy3 - entropy2));
         return null;
     }
 
-    public static void createThresholdsForAttribute(Attribute attribute){
-        TreeSet<Float> uniqueValues= new TreeSet<Float>();
-        for(String value : attribute.getValues()){
+    public static HashMap<String, Float> countOcc(final ArrayList<String> target, final ArrayList<String> attribute, final String clas) {
+        //get unique decision classes from the target
+        final Set<String> uniqueValues = new HashSet<String>(target);
+        final HashMap<String, Float> countMap = new HashMap<String, Float>();
+        //initialise hashmap with decision classes as the keys
+        for (final String className : uniqueValues) {
+            countMap.put(className, (float) 0L);
+        }
+        // count occurrences of decision classes
+        int i = 0;
+        for (final String label : target) {
+            if (attribute.get(i).equals(clas)) {
+                if (!countMap.containsKey(label)) {
+                    countMap.put(label, (float) 1L);
+                } else {
+                    Float count = countMap.get(label);
+                    count = count + 1;
+                    countMap.put(label, count);
+                }
+            }
+            i++;
+        }
+        return countMap;
+    }
+
+    public static void createThresholdsForAttribute(final Attribute attribute) {
+        final TreeSet<Float> uniqueValues = new TreeSet<Float>();
+        for (final String value : attribute.getValues()) {
             uniqueValues.add(Float.parseFloat(value));
         }
-        ArrayList<Float> a = new ArrayList<Float>(uniqueValues);
+        final ArrayList<Float> a = new ArrayList<Float>(uniqueValues);
         System.out.println(uniqueValues);
-        ArrayList<Float> t = new ArrayList<Float>();
-        for(int i =0 ; i < a.size()-1; i++){
-            t.add((a.get(i)+a.get(i+1))/2);
+        final ArrayList<Float> t = new ArrayList<Float>();
+        for (int i = 0; i < a.size() - 1; i++) {
+            t.add((a.get(i) + a.get(i + 1)) / 2);
         }
         attribute.storeThresholds(t);
         System.out.println(t);
