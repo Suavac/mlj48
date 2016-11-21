448708e785ea2e575e0f972836b3f1be21b7d5fd
SLAWOMIR KAZIMIERZ MLYNAROWICZ <12100888@IT.NUIGALWAY.IE>
2016-11-21 18:05:41 +0000

    resource missing fix

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
 
     public static void main(final String... args) throws Exception {
         //TODO - create GUI - select file, display results
-        //GUI a = new GUI();
+        GUI a = new GUI();
 
         ///http://clear-lines.com/blog/post/Discretizing-a-continuous-variable-using-Entropy.aspx
         for(int i = 0; i< 1; i++){
